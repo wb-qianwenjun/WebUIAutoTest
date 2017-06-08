d3e50b50c089e9dd76839e60c9185ae7e4e1484e
wb-qianwenjun <wb-qianwenjun@alibaba-inc.com>
2017-06-08 08:16:46 +0800

    0608modify

---
+++ src/test/java/com/qwj/test/simple/openWebdriver/OpenWedTest.java
@@ -0,0 +1,8 @@
+package com.qwj.test.simple.openWebdriver;
+
+public class OpenWedTest {
+	public static void main(String[] args){
+		Web baiduweb = new Web();
+		baiduweb.openWeb("http://www.baidu.com");
+	}
+}
