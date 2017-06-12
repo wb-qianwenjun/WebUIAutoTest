f2e0a4f03572e2808fc27f1cded3d1236db3e715
wb-qianwenjun <wb-qianwenjun@alibaba-inc.com>
2017-06-13 07:56:15 +0800

    0612commit

--- src/main/java/com/qwj/simple/webdriver/LoginCookie.java
+++ src/main/java/com/qwj/simple/webdriver/LoginCookie.java
@@ -4,13 +4,20 @@ import java.util.Iterator;
 import java.util.Set;
 
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginCookie {
-	public static void main(String[] arge){
+	public static void main(String[] arge) throws InterruptedException{
 		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().setSize(new Dimension(800,600));
 		driver.get("http://www.baidu.com");
+		
+		//清除cookie
+		driver.manage().deleteAllCookies();
 		
 		//添加cookie信息
 		Cookie cookie_baiduid = new Cookie("BAIDUID", "AFC2BC93022FD99D40E4B594396904EB:FG");
@@ -19,14 +26,27 @@ public class LoginCookie {
 		driver.manage().addCookie(cookie_baiduid);
 		driver.manage().addCookie(cookie_bduss);
 		
+		System.out.println("添加cookie之后的浏览器cookie信息");
+		printCookie(driver);
+		
+		Thread.sleep(1000);
+		driver.navigate().refresh();
+		
+		Thread.sleep(2000);
+		System.out.println("刷新之后的浏览器cookie信息");
+		printCookie(driver);
+		//清除cookie
+		driver.manage().deleteAllCookies();
+		
+//		driver.close();
+	}
+	
+	private static void printCookie(WebDriver driver){
 		Set<Cookie> cookies = driver.manage().getCookies();
 		Iterator<Cookie> i = cookies.iterator();
 		while(i.hasNext()){
 			Cookie cookie = i.next();
 			System.out.println(cookie);
 		}
-		
-		
-		driver.navigate().refresh();
 	}
 }
